17cab3ae7366505d8fceeb9a5226eb1e35cf9a76
Dan Leehr <dan.leehr@duke.edu>
2018-01-31 14:24:37 -0500

    Adds exports for singularity image dirs

--- run-trim-galore.sh
+++ run-trim-galore.sh
@@ -2,5 +2,8 @@
 
 module load singularity/2.4.2-gcb01
 source /home/dcl9/cwltool/venv/bin/activate
-
+export SINGULARITY_PULLFOLDER=/data/itlab/singularity_images
+export SINGULARITY_CACHEDIR=/data/itlab/singularity_cache
+singularity --version
 cwltool --singularity /home/dcl9/bespin-cwl/tools/trim_galore.cwl input.json
+
